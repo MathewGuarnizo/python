1ccae0a7b6928449d89c51499a5453b449d1dbb0
MathewGuarnizo <guarnizo.reyes.mathew@gmail.com>
2023-11-07 09:44:07 -0500

    update 3

---
+++ listas.py
@@ -0,0 +1,31 @@
+#punto 1
+
+Aprendices = []
+for i in range(20):
+        if i % 2 == 0:
+            Aprendices.append(i)
+print(Aprendices)
+
+Edades = []
+for i in range(20):
+    if i % 2 == 0:
+        Edades.append(i)
+print(Edades)
+
+#punto 2
+print(Aprendices)
+print(Edades)
+
+#punto 3
+Edad = max(Edades)
+print = (Edad + " " + Aprendices) 
+
+#punto 4
+Aprendices.insert(0, "Adriana Lucia")
+
+#punto 5
+Aprendices.lent
+EdAp = Aprendices + " " + str(Edades)
+
+print(EdAp)
+
